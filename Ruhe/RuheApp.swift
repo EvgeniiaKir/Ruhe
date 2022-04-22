6702440f13b2d722fc23daf6f909da0218097b51
Evgeniia Kiriushina <evgeniya.kiriushina@axonista.com>
2022-04-22 23:27:24 +0200

    Initial Commit

---
+++ Ruhe/RuheApp.swift
@@ -0,0 +1,17 @@
+//
+//  RuheApp.swift
+//  Ruhe
+//
+//  Created by Евгения Колдышева on 22.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct RuheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
